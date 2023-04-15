94a2eff48a96a4ea5362fd7ab0717dc3ffeff4c9
mohammad0x <mohammadtaghdir7@gmail.com>
2023-04-15 18:56:57 +0330

    models course

--- administrator/models.py
+++ administrator/models.py
@@ -24,13 +24,36 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     slug = models.CharField(max_length=20)
     desc = models.TextField()
-    category = models.ManyToManyField(Category , related_name="post")
+    category = models.ManyToManyField(Category, related_name="post")
     image = models.ImageField(upload_to='image/')
     publish = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
 
 
-class Course(models.Model):
-    admin_id = models.ForeignKey(MyUser , on_delete=models.CASCADE)
-    user = models.ForeignKey(MyUser , on_delete=models.CASCADE)
+class category_Course(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
+    position = models.IntegerField()
 
+    class Meta:
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    admin = models.ForeignKey(MyUser, on_delete=models.CASCADE ,related_name="adminUser")
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE , related_name="myUser")
+    category = models.ManyToManyField(category_Course, related_name="course")
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='course/')
+    price = models.IntegerField( null=False, blank=False)
+    publish = models.DateTimeField(default=timezone.now)
+
+
+class Video(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    video = models.FileField(upload_to='video/')
